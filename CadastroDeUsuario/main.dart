9b962d122f1e825a573f025128684aa81d3ef648
Jéssica Gabrielle Patro <jessi.gabriellee@gmail.com>
2020-07-16 22:48:45 -0300

    Create main.dart

---
+++ CadastroDeUsuario/main.dart
@@ -0,0 +1,21 @@
+import 'package:cadastrousuario/cadastro_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cadastro Usuario',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CadastroPage(),
+    );
+  }
+}
